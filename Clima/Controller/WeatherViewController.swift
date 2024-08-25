4e45e3a1f967b2f9406858731dfd4b1b1b05a174
anas <hello.anas07@gmail.com>
2024-08-25 11:10:05 +0300

    first commit

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,126 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
+    private var _lat: CLLocationDegrees = 0.0
+    private var _lon: CLLocationDegrees = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
 
 
 }
 
+
+
+// MARK: - CLLocationManager
+
+extension WeatherViewController: CLLocationManagerDelegate {
+
+
+
+    var lat: CLLocationDegrees {
+        get {
+            return _lat
+        }
+        set {
+            _lat = newValue
+        }
+    }
+    
+    var lon: CLLocationDegrees {
+        get {
+            return _lon
+        }
+        set {
+            _lon = newValue
+        }
+    }
+    
+    @IBAction func getCurrentWeather(_ sender: Any) {
+        weatherManager.fetchWeather(lat: lat, long: lon)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            lat = location.coordinate.latitude
+            lon = location.coordinate.longitude
+            print(location.coordinate)
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print(error)
+        print("error")
+    }
+}
+
+
+
+// MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+    @IBAction func searchPressed(_ sender: Any) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        textField.placeholder = "Type something"
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextField.text = ""
+    }
+    
+}
+
+// MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(weather: WeatherModel) {
+        print(weather.temperature)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = "\(weather.temperature)"
+            self.cityLabel.text = "\(weather.cityName)"
+            self.conditionImageView.image = UIImage(systemName: weather.condionName)
+        }
+    }
+}
---
+++ Clima/Services/WeatherManager.swift
@@ -0,0 +1,77 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Anas Salah on 14/08/2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel)
+}
+
+struct WeatherManager: WeatherManagerDelegate {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=44af51a3a0bd7db86cc6b1184dfdb6bf"
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(url: urlString)
+        print(urlString)
+    }
+
+    func fetchWeather(lat: CLLocationDegrees, long: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(lat)&lon=\(long)"
+        performRequest(url: urlString)
+        print(urlString)
+    }
+    
+    func performRequest(url: String) {
+        guard let url = URL(string: url) else { print("URL is not valid pleas check WeahterManager class line 21 in Model group and here is the url \(url)") ; return }
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            if let safeData = data { // here need to parse the data
+                if let weather = pareseJson(data: safeData) {
+                    delegate?.didUpdateWeather(weather: weather)
+                } else {
+                    print("DAta is Nil line 36 at Services")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func pareseJson(data: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: data)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            print(weather)
+            return weather
+            
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func didUpdateWeather(weather: WeatherModel) {
+        print(weather.temperature)
+    }
+}
